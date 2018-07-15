d447342d3764e97267c012cbce6bdce981fa90ee
MaryB16 <buda.maria89@gmail.com>
2018-07-15 18:43:40 +0300

    Transformed Book component into a stateless component

--- src/Book.js
+++ src/Book.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
-import * as BooksAPI from './BooksAPI'
+import React from 'react';
 import placeholder from "./icons/placeholder.jpg"
 
-class Book extends Component {
+const Book = (props) => {
+  const book= props.book
 
-  render (){
-    const book= this.props.book
+  /*Placeholders in case some details are missing from the books*/
+  const bookAuthors = book.authors ? book.authors.join(', ') : 'No authors'
+  const bookCover = book.imageLinks ? book.imageLinks.thumbnail || book.imageLinks.smallThumbnail : placeholder
+  const bookTitle = book.title? book.title : 'No title'
 
-    /*Placeholders in case some details are missing from the books*/
-    const bookAuthors = book.authors ? book.authors.join(', ') : 'No authors'
-    const bookCover = book.imageLinks ? book.imageLinks.thumbnail || book.imageLinks.smallThumbnail : placeholder
-    const bookTitle = book.title? book.title : 'No title'
-
-    return (
-      <div className="book">
-        <div className="bookshelf-changer">
-          <select value={this.props.book.shelf ? this.props.book.shelf : 'none'} onChange={(event) => {this.props.updateShelf(book, event.target.value)}}>
-            <option value="moveTo" disabled defaultValue>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+  return (
+    <div className="book">
+      <div className="bookshelf-changer">
+        <select value={props.book.shelf ? props.book.shelf : 'none'} onChange={(event) => {props.updateShelf(book, event.target.value)}}>
+          <option value="moveTo" disabled defaultValue>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+      <div className="book-details">
+        <div className="book-cover"
+          style={
+            {
+              backgroundImage:`url('${bookCover}')`
+            }}>
         </div>
-        <div className="book-details">
-          <div className="book-cover"
-            style={
-              {
-                backgroundImage:`url('${bookCover}')`
-              }}>
-          </div>
-          <div className="book-name">
-            {bookTitle}
-          </div>
-          <div className="book-authors">
-            {bookAuthors}
-          </div>
+        <div className="book-name">
+          {bookTitle}
+        </div>
+        <div className="book-authors">
+          {bookAuthors}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Book;
